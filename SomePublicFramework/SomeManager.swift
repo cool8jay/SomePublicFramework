c02f415f00760d00325fedbc5158edd80d78db4c
cool8jay <cool8jay@gmail.com>
2024-05-20 15:47:26 +0800

    Update framework.
    Update framework.

--- SomePublicFramework/SomeManager.swift
+++ SomePublicFramework/SomeManager.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-public class SomeManager : NSObject {
-    @objc public func someMethod1() {
+public class SomeManager: NSObject {
+    @objc public static func someMethod1() {
         print("someMethod1")
     }
-    
-    @objc public func someMethod2(v1: Int, v2: Int) -> Int {
+
+    @objc public static func someMethod2(v1: Int, v2: Int) -> Int {
         return v1 + v2
     }
 }
